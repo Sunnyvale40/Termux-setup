7e33580ec74c12576a9fa0b6f38c32bf3436e417
bomsayang <33796539+bomsayang@users.noreply.github.com>
2018-12-10 12:06:16 +0700

    Add files via upload

---
+++ lazy.sh
@@ -0,0 +1,52 @@
+#! /data/data/com.termux/files/usr/bin/bash
+#---All--In--One---Termux---Package-------
+apt update -y #update Termux
+apt upgrade #upgrade Termux
+apt install figlet -y #Text-design
+echo "" #blank
+echo "" #blank
+echo "" #blank
+echo "" #blank
+echo "" #blank
+echo "" #blank
+#logo of this script
+figlet "Automatically Packages Installer"
+echo "Hacker 4.0 "
+echo "Creator: Sunny"
+echo  "Facebook: Witthawin Nokkhunthong"
+echo "Subscribe Hacker 4.0 on youtube for more updates--"
+echo "Version: 1.0"
+echo "" #blank
+
+#language
+apt install python -y
+apt install python2 -y
+apt install perl -y
+apt install c -y
+apt install c++ -y
+#common pkg
+apt install git -y
+apt install nano -y
+apt install vim -y
+apt install wget -y
+apt install curl -y
+apt install zip -y
+apt install unzip -y
+apt install tar -y
+apt install rar -y
+apt install unrar -y
+apt install less -y
+#testing tools
+apt install nmap -y
+apt install hydra -y
+#git pkg
+git clone https://github.com/DedSecCyber/scorpion
+git clone https://github.com/cyweb/hammer
+git clone https://github.com/UltimateHackers/Hash-Buster
+git clone git clone https://github.com/DedSecCyber/DedSecSQL.git
+#end
+echo "####################################################################"
+echo "--------------------------------------------------------------------"
+echo "---------SUCCESSFULY INSTALLED--------------------------------------"
+echo "--------------------------------------------------------------------"
+echo "###################################################################"
